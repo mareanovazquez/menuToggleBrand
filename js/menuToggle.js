7b102de5bbce905d6267fe12b155f6088f3fb15d
mareanovazquez <mareanovazquez@protonmail.com>
2023-11-16 10:58:08 -0300

    add brand, style responsive y fontAwesome. Eliminación jQuery

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,4 +1,22 @@
-$("#toggle").click(function() {
-    $(this).toggleClass("on");
-    $("#menu").slideToggle();
-  });
+// Obtén el elemento con el id "toggle"
+let toggleButton = document.getElementById("toggle");
+
+// Agrega un evento de clic al botón
+toggleButton.addEventListener("click", function() {
+    // Toggle de la clase "on" en el botón
+    if (toggleButton.classList.contains("on")) {
+        toggleButton.classList.remove("on");
+    } else {
+        toggleButton.classList.add("on");
+    }
+
+    // Obtén el elemento con el id "menu"
+    let menu = document.getElementById("menu");
+
+    // Alternar la visibilidad del menú
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+});
